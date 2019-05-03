b223ee149aabbf1d4b970cdc2f111dccd7828da1
Bryson Spaulding <brysonian10@hotmail.com>
2019-05-03 09:28:10 -0600

    Created the two art panels and added things to them and frame.

---
+++ src/drawing/controller/DrawingController.java
@@ -0,0 +1,9 @@
+package drawing.controller;
+
+public class DrawingController
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/drawing/view/DrawingFrame.java
@@ -0,0 +1,19 @@
+package drawing.view;
+
+import javax.swing.JFrame;
+import drawing.controller.DrawingController;
+
+public class DrawingFrame
+{
+	private DrawingController app;
+	
+	public DrawingFrame()
+	{
+		super();
+	}
+	
+	private void setupFrame()
+	{
+		
+	}
+}
